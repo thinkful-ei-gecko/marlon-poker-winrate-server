ab638f1281f1828ea87792f7e2bb48f249e7b40d
Marlon Agno <mdj.agno@gmail.com>
2020-01-05 20:21:18 -0500

    recommit

--- src/app.js
+++ src/app.js
@@ -26,12 +26,12 @@ app.use('/api/user', userRouter);
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
-  if (NODE_ENV === 'production') {
-    response = { error: { message: 'server error' } };
-  } else {
+  // if (NODE_ENV === 'production') {
+  //   response = { error: { message: 'server error' } };
+  // } else {
     console.error(error);
     response = { message: error.message, error };
-  }
+  // }
   res.status(500).json(response);
 });
 
