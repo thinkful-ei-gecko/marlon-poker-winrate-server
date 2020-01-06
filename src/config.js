2216dfbe92e54ab129ee7e2fc48c71b7875c6d07
Marlon Agno <mdj.agno@gmail.com>
2020-01-05 20:25:23 -0500

    fixed typo in config

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: process.env.DATABSE_URL || 'postgresql://dunder_mifflin@localhost/poker-winrate',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/poker-winrate',
   API_BASE_URL: process.env.REACT_APP_API_BASE_URL || "http://localhost:3000/api",
   JWT_SECRET: process.env.JWT_SECRET || 'secret',
 }
